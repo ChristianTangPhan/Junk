2e420e8788f791bdd29a825a845abc31d10a6ab2
Dudwen <167839330+Dudwen@users.noreply.github.com>
2024-04-25 12:27:23 -0500

    FINAL Wheel.py
    should be the final version of this program

--- Wheel.py
+++ Wheel.py
@@ -7,20 +7,20 @@ which game
 ====================================================================
 """
 
+print("=============================")
+print("= LIST ALL GAMES PROGRAM!!! =")
+print("=============================")
+
 """
 --------------------------------------------------------------------
 Declaration
 --------------------------------------------------------------------
 """
-
 #Initializes players
-cavey, deeswa, thien, dudwen, deft = False, False, False, False, False
 ID = ["cavey", "deeswa", "thien", "dudwen", "deft"]
+status = {players: False for players in ID}
 
 #Other Initializations
-MIN = 2
-MAX = 5
-lobbySize = 0
 online = 0
 quit = False
 
@@ -31,105 +31,127 @@ gameList = ["Fortnite","Osu","Fall Guys","Warframe","Brawlhalla","Business Tour"
             "Apex","Valorant","League of Legends","Diep","Roblox","Overwatch 2","Dead by Daylight",
             "Minecraft","Party games","Moomoo.io","Mope.io","Shellshock","Spiltgate","Rouge Company",
             "Lethal Company","Rainbow Six Siege","Destiny 2","Bluestacks","Pixel Gun","Rainworld","Itchi.io",
-            "Grabity","Astroneer","Rounds", "Among Us"]
+            "Grabity","Astroneer","Rounds", "Among Us", "One Arm Robber"]
 
 #The Game Status Options
 nah = 0
 yes = 1
 d_u = 2 #Needs a download or update
-g_s = 3 #Needs a specific player to play
+own = 3 #Needs a specific player to play
 
-twoPlayer   = [9, 33]
-threePlayer = [20, 22, 36]
-fourPlayer  = [3, 4, 5, 6, 8, 12, 14, 18, 41, 43, 45]
-fivePlayer  = [23, 24]
-dudwenOnly  = [41]
+twoPlayer   = [gameList.index("Fps Chess"), gameList.index("Shellshock")]
+threePlayer = [gameList.index("Knockout City"), gameList.index("Apex Legends"), gameList.index("Rouge Company")]
+fourPlayer  = [gameList.index("Fall Guys"), gameList.index("Warframe"), gameList.index("Brawlhalla"), gameList.index("Business Tour"), 
+               gameList.index("Drunken Wrestlers 2"), gameList.index("Dauntless"), gameList.index("Phasmophobia"), gameList.index("T Mod Loader"), 
+               gameList.index("Rainworld"), gameList.index("Grabity"), gameList.index("Rounds"), gameList.index("One Arm Robber")]
+fivePlayer  = [gameList.index("Valorant"), gameList.index("League of Legends"), gameList.index("Overwatch 2"), gameList.index("Dead by Daylight"), 
+               gameList.index("Rainbow Six Siege")]
 
 """
 --------------------------------------------------------------------
-QUESTIONS 
+Questions
 --------------------------------------------------------------------
 """
-print("ALL POSSIBLE GAMES LIST!!!")
-#Asks for amount of players online
-while (lobbySize < MIN or lobbySize > MAX):
-    lobbySize = (input("How many people playing (2-5): "))
+#Drafts players
+while quit == False:
 
-    try:
-        lobbySize = int(lobbySize)
-    except ValueError:
-        lobbySize = 0
-
-    if (lobbySize < MIN or lobbySize > MAX):
-       print("Out of Range\n")
-
-#Asks if online are willing to wait for downloads or updates
-choice = input("Are you willing to download or update if needed: ")
-
-try:
-    characterCheck = str(choice)
-except ValueError:
-    choice = "nah"
-
-willing = choice.lower().startswith("y")
-if (willing == True):
-    print("Unready games will be added into this list")
-    d_u = yes
-print("")
-
-#Drafting Players
-while quit == False and online < lobbySize:
-    print("1. Cavey")
-    print("2. Deeswa")
-    print("3. Thien")
-    print("4. Dudwen")
-    print("5. Deft")
-    print("6. Thats all!")
-    player = (input("Who is playing: "))
-    
-    try:
-        player = str(player)
-    except ValueError:
-        pass
-
-    if player == "1" or player.lower() == "cavey":
-        if cavey == True:
-            print("Cavey is already added\n")
-            online -= 1
-        else:
-            cavey = True
-            print("Cavey is added\n")
-    elif player == "2" or player.lower() == "deeswa":
-        if deeswa == player == "deeswa":
-            print("Deeswa is already added\n")
-            online -= 1
-        else:
-            deeswa = True
-            print("Deeswa is added\n")
-    elif player == "3" or player.lower() == "thien":
-        if thien == True:
-            print("thien is already added\n")
-            online -= 1
-        else:
-            thien = True
-            print("Thien is added\n")
-    elif player == "4" or player.lower() == "dudwen":
-        if dudwen == True:
-            print("Dudwen is already added\n")
-            online -= 1
-        else:
-            dudwen = True
-            print("Dudwen is added\n")
-    elif player == "5" or player.lower() == "deft":
-        if deft == True:
-            print("Deft is already added\n")
-            online -= 1
-    elif player == "6" or player.lower() == "quit":
-        quit == True
+    #Checks if everyone in list is already added
+    falseCheck = True
+    for players in range(len(ID)):
+        if status[ID[players]] == False:
+            falseCheck = False
+    if falseCheck == False:
+        quit = False
     else:
-        print("not an option\n")
-        online -= 1
-    online += 1
+        quit = True
+
+    if quit == False:
+        print("\n========== MENU ==========")
+        print("1. Cavey")
+        print("2. Deeswa")
+        print("3. Thien")
+        print("4. Dudwen")
+        print("5. Deft")
+        print("6. Who's in the -party-?")
+        print("7. Reset party")
+        print("8. Thats all!")
+        player = (input("\nWho is playing: "))
+        player = str(player)
+
+        if player == "1" or player.lower() == "cavey":
+            if status["cavey"] == True:
+                print("\n-- Cavey is already added --")
+            else:
+                status["cavey"] = True
+                print("\n-- Cavey is added --")
+                online += 1
+        
+        elif player == "2" or player.lower() == "deeswa":
+            if status["deeswa"] == True:
+                print("\n-- Deeswa is already added --")
+            else:
+                status["deeswa"] = True
+                print("\n-- Deeswa is added --")
+                online += 1
+        
+        elif player == "3" or player.lower() == "thien":
+            if status["thien"] == True:
+                print("\n-- thien is already added --")
+            else:
+                status["thien"] = True
+                print("\n-- Thien is added --")
+                online += 1
+        
+        elif player == "4" or player.lower() == "dudwen":
+            if status["dudwen"] == True:
+                print("\n-- Dudwen is already added --")
+            else:
+                status["dudwen"] = True
+                print("\n-- Dudwen is added --")
+                online += 1
+        
+        elif player == "5" or player.lower() == "deft":
+            if status["deft"] == True:
+                print("\n-- Deft is already added --")
+            else:
+                status["deft"] = True
+                print("\n-- Deft is added --")
+                online += 1
+        
+        elif player == "6" or player.lower() == "party":
+            print("\n== These players are in the party ==")
+            if online == 0:
+                print("No one")
+            elif online > 0:
+                for players in range(len(ID)):
+                    if status[ID[players]] == True:
+                        print(ID[players].capitalize(), end = ' ') 
+            else:
+                print("\n\nhow did you break the program this bad...")
+            print("")
+        
+        elif player == "7" or player.lower() == "reset":
+            for players in range(len(ID)):
+                status[ID[players]] = False
+            print("\n<< Party is reset >>")
+            online = 0
+        
+        elif player == "8" or player.lower() == "quit":
+            if online < 2:
+                print("\n!! Not enough !!")
+            else:
+                quit = True
+        else:
+            print("\n!! Not option !!")
+
+#Asks if online players are willing to wait for downloads or updates
+choice = input("\nAre yall willing to download or update games: ")
+characterCheck = str(choice)
+willing = choice.lower().startswith("y")
+
+if willing == True:
+    print("<< Unready games added to list >>")
+    d_u = yes
 
 """
 --------------------------------------------------------------------
@@ -137,7 +159,7 @@ Database
 --------------------------------------------------------------------
 """
 # Order: Cavey, Deeswa, Thien, Dudwen, Deft
-# Contains players status on specified games
+# Contains players option on each games
 database = [
     # 1. Fortnite
     [yes, yes, yes, yes, d_u],
@@ -231,59 +253,62 @@ database = [
     [yes, yes, yes, yes, yes],
     # 46. Among Us
     [d_u, yes, yes, yes, yes],
+    # 47. One Arm Robber
+    [yes, yes, yes, yes, yes],
 ]
 
 """
 --------------------------------------------------------------------
-CALCUATIONS
+Calculations
 --------------------------------------------------------------------
 """
-#Removes offline from list
-if cavey == False:
-    for reset in range(len(database)):
-        database[reset][ID.index("cavey")] = yes
-if deeswa == False:
-    for reset in range(len(database)):
-        database[reset][ID.index("deeswa")] = yes
-if thien == False:
-    for reset in range(len(database)):
-        database[reset][ID.index("thien")] = yes
-if dudwen == False:
-    for reset in range(len(database)):
-        database[reset][ID.index("dudwen")] = yes
-if deft == False:
-    for reset in range(len(database)):
-        database[reset][ID.index("deft")] = yes
+#Congrats if you can understand this (@_@)
+for games in range(len(database)):
+    for scan in range(len(database[games])):
+        if database[games][scan] == own:
+            if status[ID[scan]] == True:
+                database[games][scan] = yes
 
-#Needs Dudwen to play
-if dudwen == False:
-    for players in range(len(ID)):
-        for remove in range(len(dudwenOnly)):
-            database[dudwenOnly[remove]-1][players] = nah
+#Removes offline players from list
+if status["cavey"] == False:
+    for remove in range(len(database)):
+        database[remove][ID.index("cavey")] = yes
+if status["deeswa"] == False:
+    for remove in range(len(database)):
+        database[remove][ID.index("deeswa")] = yes
+if status["thien"] == False:
+    for remove in range(len(database)):
+        database[remove][ID.index("thien")] = yes
+if status["dudwen"] == False:
+    for remove in range(len(database)):
+        database[remove][ID.index("dudwen")] = yes
+if status["deft"] == False:
+    for remove in range(len(database)):
+        database[remove][ID.index("deft")] = yes
 
 #Removes 2 player games
 if online > 2:
     for players in range(len(ID)):
         for remove in range(len(twoPlayer)):
-            database[twoPlayer[remove]-1][players] = nah
+            database[twoPlayer[remove]][players] = nah
 """
 #Removes 3 player games
 if online > 3:
     for players in range(len(ID)):
         for remove in range(len(threePlayer)):
-            database[threePlayer[remove]-1][players] = nah
+            database[threePlayer[remove]][players] = nah
 """
 #Removes 4 player games
 if online > 4:
     for players in range(len(ID)):
         for remove in range(len(fourPlayer)):
-            database[fourPlayer[remove]-1][players] = nah
+            database[fourPlayer[remove]][players] = nah
 """
 #Removes 5 player games
 if online > 5:
     for players in range(len(ID)):
         for remove in range(len(fivePlayer)):
-            database[fivePlayer[remove]-1][players] = nah
+            database[fivePlayer[remove]][players] = nah
 """
 
 """
